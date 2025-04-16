6e1703c19e2074e5d3f6fe9552adb360d22c62c6
Rokkam <rokkamanu542@gmail.com>
2025-04-16 18:24:58 +0530

    redux

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,8 +1,14 @@
-
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, removeItem, clearCart } from '../utils/AppSlice';
 const Login = () => {
+  
   return (
-    <div>Login</div>
+    <div>
+    <button >Login</button>
+    
+    </div>
   )
 }
 
-export default Login
+export default Login;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from "react-redux";
+import appStore from "./utils/AppStore";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={appStore}>
+   
+  
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </Provider>
+  
+
 )
